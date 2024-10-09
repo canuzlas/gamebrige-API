77360f549eb1ce0d28043f23855e2d4209d084f4
canuzlas <79049730+canuzlas@users.noreply.github.com>
2024-10-10 01:45:08 +0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 const mongodb = require("mongoose")
-mongodb.connect('mongodb+srv://mcuzlas:1MTLt9GNO6CtPHyD@cluster0.tc0z9js.mongodb.net/gamebrige?retryWrites=true&w=majority',{
+mongodb.connect('mongodb+srv://**:**@cluster0.tc0z9js.mongodb.net/gamebrige?retryWrites=true&w=majority',{
    useNewUrlParser: true,
    useUnifiedTopology: true
 }).then(success => console.log("mongoDB bağlandı"))
